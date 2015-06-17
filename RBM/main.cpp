ceb7dff728f9fcd4bae50fd70e94e5fc521df9a8
Adam Sanghera <support@Supports-MBP.wp.comcast.net>
2015-06-17 13:38:46 -0700

    Initial Commit

---
+++ RBM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RBM
+//
+//  Created by Adam Sanghera on 6/17/15.
+//  Copyright (c) 2015 adamSanghera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
