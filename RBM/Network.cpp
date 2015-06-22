170e09cf99dfbf14e5d60e11e591cd0103216597
as8513 <as8513@nyu.edu>
2015-06-22 10:44:37 -0700

    Begins brainstorming the implementation of iterative learning.  No code has been written, only pseudocode.

--- RBM/Network.cpp
+++ RBM/Network.cpp
@@ -27,3 +27,21 @@ void Boltzmann::Network::pushLayer(Boltzmann::Layer l) {
     listOfWeightMatrices.push_back(Matrix(listOfLayers[numLayers-1]->numUnits, l.numUnits));
     numLayers++;
 }
+
+//  Replaces the current input vector (visible base layer) with a new input vector
+
+void Boltzmann::Network::swapOutInput(std::vector<bool> inputs) {
+    for (int i = 0; i < listOfLayers[0]->numUnits; ++i) {
+        listOfLayers[0]->clampState(i, inputs[i]);
+    }
+}
+
+void Boltzmann::Network::iterateLearnCycle(double learningRate) {
+    // What do we have to do here?
+    //  1.  Iterate up the network, pinging from left to right.  We should save the 'pinged' states in some temporary vector.
+    //  2.  Save this state of the network somewhere. 1st state
+    //  3.  Iterate *down* the network, pinging from left to right.  Treat the topmost units as clamped, generate next layer down, clamp. so on.
+    //  4.  Save this state of the network somewhere. 2nd state
+    //  5.  Create a matrix for each incremental layer pair for both network state 1 and 2, tracking the off-on states.
+    //  6.  Iterate through the weight matrices, and subtract from them these off-on matrices times a learning factor.
+}
